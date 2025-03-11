d6124065bba374da2b10a82402aab0c18308a243
Minh Đức <mducbg2000@gmail.com>
2025-03-11 11:51:00 +0700

    Add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+export default [...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')]
