468c802b8b697f5635b77f6346e947b69c8b4857
Minh Đức <mducbg2000@gmail.com>
2025-03-11 10:06:59 +0700

    Add PayloadCMS

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,14 +1,14 @@
 import { Inter, Outfit } from "next/font/google";
-import "../../public/assets/css/animate.min.css";
-import "../../public/assets/css/bootstrap.min.css";
-import "../../public/assets/css/flaticon.css";
-import "../../public/assets/css/fontawesome-all.min.css";
-import "../../public/assets/css/magnific-popup.css";
-import "../../public/assets/css/odometer.css";
-import "../../public/assets/css/swiper-bundle.css";
-import "../../public/assets/css/aos.css";
-import "../../public/assets/css/default.css";
-import "../../public/assets/css/main.css";
+import "../../../public/assets/css/animate.min.css";
+import "../../../public/assets/css/bootstrap.min.css";
+import "../../../public/assets/css/flaticon.css";
+import "../../../public/assets/css/fontawesome-all.min.css";
+import "../../../public/assets/css/magnific-popup.css";
+import "../../../public/assets/css/odometer.css";
+import "../../../public/assets/css/swiper-bundle.css";
+import "../../../public/assets/css/aos.css";
+import "../../../public/assets/css/default.css";
+import "../../../public/assets/css/main.css";
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
